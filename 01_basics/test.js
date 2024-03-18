0ca80bf4fb9e1c6940f5fc3af10aa68d8db7c2f5
Archit lakhera <163811627+lakheraArchit@users.noreply.github.com>
2024-03-18 07:01:15 +0000

    datatypes

--- 01_basics/test.js
+++ 01_basics/test.js
@@ -1,1 +1,10 @@
-console.log("archit");
+const accountID = 45367;
+let username ="archit.april@outlook.com";
+var password = "761811";
+
+//accountID = 56879
+accountId = 56789;
+username ="lakheraarchit85@gmail.com";
+password ="991752";
+
+console.table([accountId,username,password]);
